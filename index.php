932623310f8156a34387c70153ef249dfaa6b36f
Dnadapierre <photostudio13000@gmail.com>
2019-09-30 11:17:28 +0200

    ajout structure de base lien meta link css

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Charte des apprenant.e.s</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1 class="titreP">Charte des apprenant.e.s</h1>
+		<div class="section1"></div>
+		<div class="section2"></div>
+		<div class="section3"></div>
+	</div>
+	
+</body>
+</html>
