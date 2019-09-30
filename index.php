0148bc7b04a660d69dc4236d96bc2f8d2b07fbef
Dnadapierre <photostudio13000@gmail.com>
2019-09-30 11:54:15 +0200

    ajout du contenu html liste et charte

--- index.php
+++ index.php
@@ -8,9 +8,48 @@
 <body>
 	<div class="container">
 		<h1 class="titreP">Charte des apprenant.e.s</h1>
-		<div class="section1"></div>
-		<div class="section2"></div>
-		<div class="section3"></div>
+		<div class="section1">
+			<h2 class="sousTitre">Fonctionnement global de la promo / Boon of the tiger</h2>
+			<ul class="listChart">
+				<li>le confort de tou.te.s en compte tu prendras</li>
+				<li>des locaux propres tu préserveras</li>
+				<li>collectivement le vendredi tu rangeras</li>
+				<li>ta météo brièvement tu feras</li>
+				<li>la veille tu respecteras</li>
+				<li>la feuille de présence tu signeras</li>
+				<li>dans le travail concentré tu seras</li>
+			</ul>
+		</div>
+		<div class="section2">
+			<h2 class="sousTitre">Etat d'esprit / vision / Hidden war</h2>
+			<ul class="listChart">
+				<li>à la réussite collective tu oeuvreras</li>
+				<li>plus haut que ton cul ne pèteras</li>
+				<li>quand la parole tu prendras l'attention tu auras</li>
+				<li>au bien être de tous tu veilleras</li>
+				<li>dans le partage tu travailleras</li>
+				<li>avec patience et persévérance tu apprendras</li>
+				<li>ton utopote tu respecteras</li>
+				<li>ton esprit ouvert tu garderas</li>
+				<li>en autonomie tu feras mais l'esprit de groupe tu protègeras</li>
+			</ul>
+		</div>
+		<div class="section3">
+			<h2 class="sousTitre">
+				Engagements de l'apprenant.e / ligthningcrushers
+			</h2>
+			<ul class="listChart">
+				<li>assidu tu seras</li>
+				<li>le temps de parole tu respecteras</li>
+				<li>tes formateurs tu respecteras</li>
+				<li>l'entraide tu pratiqueras</li>
+				<li>ta curiosité tu aiguiseras</li>
+				<li>ta connaissance constamment tu élargiras</li>
+				<li>dans tes recherches, méthodique tu seras</li>
+				<li>ces règles tu appliqueras</li>
+				<li>le matériel tu respecteras</li>
+			</ul>
+		</div>
 	</div>
 	
 </body>
