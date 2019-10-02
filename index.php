fca271ec5970e71b71de55cb338c2fa8353fa89a
Dnadapierre <photostudio13000@gmail.com>
2019-10-02 23:13:26 +0200

    reprise de l'indentation

--- index.php
+++ index.php
@@ -3,74 +3,77 @@ include 'db.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>Charte des apprenant.e.s</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="UTF-8">
+  <title>Charte des apprenant.e.s</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
+
 <body>
-	<h1 class="titreP">Charte des apprenant.e.s</h1>
-	<div class="container">
-		<div class="section1">
-			<h2 class="sousTitre">Fonctionnement global de la promo / Boon of the tiger</h2>
-			<ul class="listChart">
-				<li>_ le confort de tou.te.s en compte tu prendras</li>
-				<li>_ des locaux propres tu préserveras</li>
-				<li>_ collectivement le vendredi tu rangeras</li>
-				<li>_ ta météo brièvement tu feras</li>
-				<li>_ la veille tu respecteras</li>
-				<li>_ la feuille de présence tu signeras</li>
-				<li>_ dans le travail concentré tu seras</li>
-			</ul>
-		</div>
-		<div class="section2">
-			<h2 class="sousTitre">Etat d'esprit / vision / Hidden war</h2>
-			<ul class="listChart">
-				<li>_ à la réussite collective tu oeuvreras</li>
-				<li>_ plus haut que ton cul ne pèteras</li>
-				<li>_ quand la parole tu prendras l'attention tu auras</li>
-				<li>_ au bien être de tous tu veilleras</li>
-				<li>_ dans le partage tu travailleras</li>
-				<li>_ avec patience et persévérance tu apprendras</li>
-				<li>_ ton utopote tu respecteras</li>
-				<li>_ ton esprit ouvert tu garderas</li>
-				<li>_ en autonomie tu feras mais l'esprit de groupe tu protègeras</li>
-			</ul>
-		</div>
-		<div class="section3">
-			<h2 class="sousTitre">
-				Engagements de l'apprenant.e / ligthningcrushers
-			</h2>
-			<ul class="listChart">
-				<li>_ assidu tu seras</li>
-				<li>_ le temps de parole tu respecteras</li>
-				<li>_ tes formateurs tu respecteras</li>
-				<li>_ l'entraide tu pratiqueras</li>
-				<li>_ ta curiosité tu aiguiseras</li>
-				<li>_ ta connaissance constamment tu élargiras</li>
-				<li>_ dans tes recherches, méthodique tu seras</li>
-				<li>_ ces règles tu appliqueras</li>
-				<li>_ le matériel tu respecteras</li>
-			</ul>
-		</div>	
-	</div>
-	<form action="index.php" method="post" class="form-study">
-		  <div class="form-study padLabel">
-		    <label for="name">Enter Name: </label>
-		    <input type="text" name="name" id="name" required>
-		  </div>
-		  <div class="form-study padButton">
-		    <input type="submit" name="sign" value="Sign!">
-		  </div>
-		</form>
-		<div class="allNames">
-<?php 
+  <h1 class="titreP">Charte des apprenant.e.s</h1>
+  <div class="container">
+    <div class="section1">
+      <h2 class="sousTitre">Fonctionnement global de la promo / Boon of the tiger</h2>
+      <ul class="listChart">
+        <li>_ le confort de tou.te.s en compte tu prendras</li>
+        <li>_ des locaux propres tu préserveras</li>
+        <li>_ collectivement le vendredi tu rangeras</li>
+        <li>_ ta météo brièvement tu feras</li>
+        <li>_ la veille tu respecteras</li>
+        <li>_ la feuille de présence tu signeras</li>
+        <li>_ dans le travail concentré tu seras</li>
+      </ul>
+    </div>
+    <div class="section2">
+      <h2 class="sousTitre">Etat d'esprit / vision / Hidden war</h2>
+      <ul class="listChart">
+        <li>_ à la réussite collective tu oeuvreras</li>
+        <li>_ plus haut que ton cul ne pèteras</li>
+        <li>_ quand la parole tu prendras l'attention tu auras</li>
+        <li>_ au bien être de tous tu veilleras</li>
+        <li>_ dans le partage tu travailleras</li>
+        <li>_ avec patience et persévérance tu apprendras</li>
+        <li>_ ton utopote tu respecteras</li>
+        <li>_ ton esprit ouvert tu garderas</li>
+        <li>_ en autonomie tu feras mais l'esprit de groupe tu protègeras</li>
+      </ul>
+    </div>
+    <div class="section3">
+      <h2 class="sousTitre">
+        Engagements de l'apprenant.e / ligthningcrushers
+      </h2>
+      <ul class="listChart">
+        <li>_ assidu tu seras</li>
+        <li>_ le temps de parole tu respecteras</li>
+        <li>_ tes formateurs tu respecteras</li>
+        <li>_ l'entraide tu pratiqueras</li>
+        <li>_ ta curiosité tu aiguiseras</li>
+        <li>_ ta connaissance constamment tu élargiras</li>
+        <li>_ dans tes recherches, méthodique tu seras</li>
+        <li>_ ces règles tu appliqueras</li>
+        <li>_ le matériel tu respecteras</li>
+      </ul>
+    </div>
+  </div>
+  <form action="index.php" method="post" class="form-study">
+    <div class="form-study padLabel">
+      <label for="name">Enter Name: </label>
+      <input type="text" name="name" id="name" required>
+    </div>
+    <div class="form-study padButton">
+      <input type="submit" name="sign" value="Sign!">
+    </div>
+  </form>
+  <div class="allNames">
+    <?php 
 	while($donnees = $result->fetch())
 {
 	echo '<p>'.htmlspecialchars($donnees['nom']).'</p>';
 }
 ?>
-		</div>	
+  </div>
 </body>
+
 </html>
