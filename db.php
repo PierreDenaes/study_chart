518a18527237883ae352e0adcb63a677b9cf881f
Dnadapierre <photostudio13000@gmail.com>
2019-10-04 14:27:39 +0200

    integration protection htmlspecialchars

--- db.php
+++ db.php
@@ -5,16 +5,18 @@ try {
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
-// Insertion du nom à l'aide d'une requête préparée
+// Vérification et Insertion du nom à l'aide d'une requête préparée
 if (isset ($_POST['sign'])){
+    //Vérification du nom dans la base de donnée
                 $name = $_POST['name'];
                 $res_n = $dbh->query("SELECT * FROM infos_tbl WHERE nom='$name'");
                 $existName = $res_n->rowCount();
+                //Insertion du nom dans la base de donnée
                 if($existName == 0) {
-                	$sth = $dbh->prepare("INSERT INTO infos_tbl (nom) VALUES(?)");
-                	$sth->execute(array($_POST['name']));
+                    $sth = $dbh->prepare("INSERT INTO infos_tbl (nom) VALUES(?)");
+                    $sth->execute(array($_POST['name']));
                 }else {
-                	echo "Cette personne existe déja";
+                    echo "Cette personne existe déja";
                 }
             }
           
