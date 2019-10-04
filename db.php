9c5abeb5abf8df22d323903b70b092ee86195391
Dnadapierre <photostudio13000@gmail.com>
2019-10-04 15:14:40 +0200

    ajout protection injection mysql sur PDO

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
 <?php
  // Connexion à la base de données
 try {
-    $dbh = new PDO("mysql:host=localhost;dbname=nom_signataire", "root", "root");
+    $dbh = new PDO("mysql:host=localhost;dbname=nom_signataire", "root", "root",[
+        PDO::ATTR_EMULATE_PREPARES => false, 
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
